d0d3fc726a3bcc82dd2940c80d714ba44a0858d6
Delight Sheriff <delightsheriff@gmail.com>
2024-05-19 12:49:31 +0100

    fixed web tokens

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -2,4 +2,6 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT,
   ALLOWED_EMAILS: process.env.ALLOWED_EMAILS,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY,
 };
